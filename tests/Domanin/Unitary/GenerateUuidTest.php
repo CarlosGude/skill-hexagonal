fcf58c127eac0a19d5d67e0d2b6aae062479da10
Carlos Gude Sánchez <carlos.sgude@gmail.com>
2023-04-17 16:02:38 +0200

    fix: Validate Uuid generator

--- tests/Domanin/Unitary/GenerateUuidTest.php
+++ tests/Domanin/Unitary/GenerateUuidTest.php
@@ -10,11 +10,11 @@ class GenerateUuidTest extends KernelTestCase
     /**
      * @throws \Exception
      */
-    public function validateUuid(): void
+    public function testValidateUuid(): void
     {
         $uuid = GenerateUuid::generate();
 
-        $valid = !preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/', $uuid);
+        $valid = preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/', $uuid) != false;
 
         $this->assertIsString($uuid);
         $this->assertTrue($valid);
