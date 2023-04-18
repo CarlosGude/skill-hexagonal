3d21a97b9b0e99b1eb330b5861fcffd7b4a9e18b
Carlos Gude Sánchez <carlos.sgude@gmail.com>
2023-04-18 15:40:01 +0200

    fix: Style

--- tests/Domanin/Unitary/GenerateUuidTest.php
+++ tests/Domanin/Unitary/GenerateUuidTest.php
@@ -14,7 +14,7 @@ class GenerateUuidTest extends KernelTestCase
     {
         $uuid = GenerateUuid::generate();
 
-        $valid = preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/', $uuid) != false;
+        $valid = false != preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/', $uuid);
 
         $this->assertIsString($uuid);
         $this->assertTrue($valid);
